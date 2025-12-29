be48cbd4a80e12ba4b3b37951c757d007422954f
Chase Alexander <ddsbss404@gmail.com>
2025-12-29 17:28:55 -0600

    Update ASCII file path in main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod info;
 mod render;
 
 fn main() {
-    let ascii = ascii::load_ascii("src/proto");
+    let ascii = ascii::load_ascii("/usr/local/bin//proto");
     let info_lines = info::all();
 
     render::print(&ascii, &info_lines);
